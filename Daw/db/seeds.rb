424155667c8fc7dab99ce16302c09fa9203cd3b4
Anthony Herrera <ankehecr100@gmail.com>
2019-02-05 17:12:47 -0500

    ...

--- Daw/db/seeds.rb
+++ Daw/db/seeds.rb
@@ -5,4 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Calificacion.create([{nota: 100, estudiante_id: '922657571', curso_id: 'prepolitecnico'}])
+#Calificacion.create([{nota: 100, estudiante_id: '922657571', curso_id: 'prepolitecnico'}])
+Materium.create!(codigo: '001', nombre: 'Matematicas', descripcion: 'conceptos fundamentales de matematicas')
+Profesor.create!(cedula: '931411438', nombres: 'Joaquin',apellidos: 'Piguave')
+#Curso.create!(id_curso: 'sb', nombre: 'Ser bachiller', duracion: '3 meses', materium_id: '001', profesor_id: '931411438')
