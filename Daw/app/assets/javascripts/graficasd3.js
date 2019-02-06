41c0ddf38adbccc7b5ace2712fba4a897a0d5acc
JuanJose <juajoflo@espol.edu.ec>
2019-02-06 00:12:37 -0500

    form

--- Daw/app/assets/javascripts/graficasd3.js
+++ Daw/app/assets/javascripts/graficasd3.js
@@ -1,29 +1,38 @@
 var data = [10, 5, 12, 15];
          
-         var width = 300 
+         var width = 600, 
             scaleFactor = 20, 
-            barHeight = 30;
+            height = 500,
+            barPadding = 10;
+         var barWidth = (width/data.length);
          
          var graph = d3.select("#g1")
             .append("svg")
             .attr("width", width)
-            .attr("height", barHeight * data.length);
+            .attr("height", height);
          
          var bar = graph.selectAll("g")
             .data(data)
             .enter()
             .append("g")
             .attr("transform", function(d, i) {
-               return "translate(0," + i * barHeight + ")";
+               var translate = [barWidth * i,0];
+               return "translate(" + translate+ ")";
+            })
+            .attr("y", function(d){
+               return height-d;
             });
+
          bar.append("rect").attr("width", function(d) {
             return d * scaleFactor;
          })
-         
-         .attr("height", barHeight - 1);
+         .attr("height", barWidth-barPadding);
+            
          
          bar.append("text")
-            .attr("x", function(d) { return (d*scaleFactor); })
-            .attr("y", barHeight / 2)
-            .attr("dy", ".35em")
+            .attr("y", function(d) { return (d*scaleFactor); })
+            .attr("x", barWidth / 2)
+            .attr("dx", ".35em")
             .text(function(d) { return d; });
+
+         
