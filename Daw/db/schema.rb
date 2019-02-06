f35ae788528207320e815bca6aa9e3743049c9b9
Gabodp <gabparedes@hotmail.com>
2019-02-06 15:11:35 -0500

    laso

--- Daw/db/schema.rb
+++ Daw/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190206152618) do
+ActiveRecord::Schema.define(version: 20190206164404) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -81,10 +81,10 @@ ActiveRecord::Schema.define(version: 20190206152618) do
   end
 
   create_table "inscripcions", force: :cascade do |t|
-    t.integer "estudiante_id"
-    t.integer "curso_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "estudiante_id"
+    t.string "curso_id"
     t.index ["curso_id"], name: "index_inscripcions_on_curso_id"
     t.index ["estudiante_id"], name: "index_inscripcions_on_estudiante_id"
   end
