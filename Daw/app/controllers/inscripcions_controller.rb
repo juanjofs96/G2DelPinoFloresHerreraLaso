a8ba6312e5b25a2b34c2633a6639e0b9fdf9c944
Anthony Herrera <ankehecr100@gmail.com>
2019-02-06 17:21:21 -0500

    cursos

--- Daw/app/controllers/inscripcions_controller.rb
+++ Daw/app/controllers/inscripcions_controller.rb
@@ -13,6 +13,14 @@ class InscripcionsController < ApplicationController
   	redirect_to estudiantes_cuenta_path
   end
 
+  def destroy
+    #@estudiante = Estudiante.find(params[:estudiante_id])
+    #@inscripcion = @estudiante.inscripcions.find(params[:id])
+    @inscripcion = Inscripcion.find(params[:id])
+    @inscripcion.destroy
+    redirect_to estudiantes_cuenta_path
+  end
+
   private
   def inscripcion_params
     params.require(:inscripcion).permit(:estudiante_id, :curso_id)
